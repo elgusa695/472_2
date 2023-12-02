55bef6d674f56d864e57bb314bd74c35402a5ec5
as <as>
2023-12-02 12:39:06 -0500

    ffff

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,7 +1,7 @@
 /**
  * CONFIGURACION 
  */
-    const API_URL = 'https://d31ea0vwkhqfoi.cloudfront.net'; // Cambiar según convenga.
+    const API_URL = 'https://d1lm5f8qczljcu.cloudfront.net'; // Cambiar según convenga.
 
 
 
